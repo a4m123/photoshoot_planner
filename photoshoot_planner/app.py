112cf17e184673348eeec90dd88d0753b79b3450
a4m123 <quasaq69@gmail.com>
2025-07-29 12:29:55 +0700

    add dark mode

--- photoshoot_planner/app.py
+++ photoshoot_planner/app.py
@@ -357,4 +357,4 @@ def delete_user(user_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5005, host='0.0.0.0')
+    app.run(debug=True, port=5010, host='0.0.0.0')
